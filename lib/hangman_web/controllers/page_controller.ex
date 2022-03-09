f3f493ed35aa139d77469722b5b0272629531028
Michael Smith <michael@msmith.nz>
2022-03-09 15:26:36 +1300

    created new game

--- lib/hangman_web/controllers/page_controller.ex
+++ lib/hangman_web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule HangmanWeb.PageController do
   use HangmanWeb, :controller
+  alias Hangman.{Repo, Game}
 
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  def create_game(conn, _params) do
+    Repo.insert(%Game{word: "elixir"})
+
+    conn
+    |>put_flash(:info, "created new game")
+    |> render("index.html")
   end
 end
--- lib/hangman_web/router.ex
+++ lib/hangman_web/router.ex
@@ -18,6 +18,7 @@ defmodule HangmanWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    post "/", PageController, :create_game
   end
 
   # Other scopes may use custom stacks.
